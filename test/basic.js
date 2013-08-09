15d3900b497d74922f6e5b5cc1d3712be11eb925
Brian Link <cpsubrian@gmail.com>
2013-08-09 11:45:28 -0700

    Adds implementation and simple test.

--- test/basic.js
+++ test/basic.js
@@ -1,3 +1,31 @@
 describe('basic test', function () {
-  it('works');
+  var app;
+
+  before(function (done) {
+    app = require('cantina');
+    app.boot(function (err) {
+      if (err) return done(err);
+      app.silence();
+      require('../');
+      app.start(done);
+    });
+  });
+
+  after(function (done) {
+    app.destroy(done);
+  });
+
+  it('can schedule a job', function () {
+    var job;
+    app.cron.schedule({
+      title: 'Test One',
+      cronTime: '* * * * *',
+      queue: 'test:one',
+      payload: {test: 'one'}
+    });
+    assert.equal(app.cron.jobs.length, 1);
+    job = app.cron.jobs.pop();
+    assert.equal(job.title, 'Test One');
+  });
+
 });
